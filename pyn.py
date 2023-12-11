fc99c43d4292f159746d4e62131ad471d5781879
unknown <sadebimpe21@gmail.com>
2023-12-11 17:06:29 +0100

    my first commit

---
+++ pyn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:44:58 2023
+
+@author: samuel
+"""
+
+import pyn2
+
+pyn2.saysomething()
+
+print ('hello world')
---
+++ pyn2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:46:38 2023
+
+@author: samuel
+"""
+
+def saysomething():
+    print('something')
